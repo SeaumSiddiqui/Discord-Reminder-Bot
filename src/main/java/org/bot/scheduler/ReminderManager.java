85453c08013a23495e3e1b73fd240ebd62cb0861
Seaum Siddiqui <seaumsiddiqui@yahoo.com>
2023-11-21 02:18:37 +0600

    Added manager class and command interface inorder-to control commands precisely

---
+++ src/main/java/org/bot/scheduler/ReminderManager.java
@@ -0,0 +1,42 @@
+package org.bot.scheduler;
+
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReminderManager extends ListenerAdapter {
+    private final List<ICommand> commands = new ArrayList<>();
+
+    @Override
+    public void onReady(ReadyEvent event) {
+        for (Guild guild: event.getJDA().getGuilds()) {
+            for (ICommand command: commands) {
+                guild
+                        .upsertCommand(
+                                command.getName(),
+                                command.getDescription())
+                        .addOptions(command.getOptions())
+                        .queue();
+            }
+        }
+    }
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        for (ICommand command: commands) {
+            if (command.getName().equals(event.getName())) {
+                command.execute(event);
+                return;
+            }
+        }
+    }
+
+    public void addCommand(ICommand command) {
+        commands.add(command);
+    }
+}
